8d380ef9773338e3525612225c801669c6fb125a
Kayan Florencio da Silva <kayan.silva@iterative.com.br>
2020-02-15 23:26:24 -0300

    Projeto inical em Angular, que emoção!

---
+++ alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alurapic';
+  url = "https://i0.wp.com/gamelogia.com.br/wp-content/uploads/2016/11/jogos-30.jpg?fit=640%2C960&ssl=1";
+  descricao = "Gamesss2"
+}
