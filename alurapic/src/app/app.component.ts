f900f82e4e9a1812cfae48fc500148f17fad4773
Kayan Florencio <kayanf.silva@outlook.com>
2020-03-03 22:30:09 -0300

    Metade da primeira aula

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -6,7 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
-  url = "https://i0.wp.com/gamelogia.com.br/wp-content/uploads/2016/11/jogos-30.jpg?fit=640%2C960&ssl=1";
-  descricao = "Gamesss2"
 }
