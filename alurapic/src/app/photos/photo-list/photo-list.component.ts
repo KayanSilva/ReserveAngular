a9697ad031783b954d8097a275b55f55b102f8f7
Kayan Florencio <kayanf.silva@outlook.com>
2020-06-09 23:27:07 -0300

    Finalizado aulas 5 e 6

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,27 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
-  photos: object[] = [];
+export class PhotoListComponent implements OnInit, OnDestroy {
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+  photos: Photo[] = [];
+  filter = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore = true;
+  currentPage = 1;
+  userName = '';
+
+  constructor(private activatedRoute: ActivatedRoute, private photoService: PhotoService) { }
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data.photos;
 
-    const username = this.activatedRoute.snapshot.params.userName;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
 
-    this.photoService.listFromUser(username).subscribe(
-      photos => this.photos = photos,
-      err => console.error(err.message));
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPagined(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      });
   }
 
 }
