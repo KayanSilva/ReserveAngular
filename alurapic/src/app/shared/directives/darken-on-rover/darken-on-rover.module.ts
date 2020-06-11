5a33472ae9f25ea999cf0b1113171f4556bc6cf1
Kayan Florencio <kayanf.silva@outlook.com>
2020-06-11 01:43:21 -0300

    Finalizado Fundamentos do Angular

---
+++ alurapic/src/app/shared/directives/darken-on-rover/darken-on-rover.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { DarkenOnHoverDirective } from './darken-on-rover.directive';
+
+@NgModule({
+    declarations: [
+        DarkenOnHoverDirective
+    ],
+    exports: [
+        DarkenOnHoverDirective
+    ]
+})
+export class DarkenOnHoverModule { }
