084d49ba21c1bc01269a7a2f5086bd85fb23601a
Kayan Florencio <kayanf.silva@outlook.com>
2021-02-21 20:33:32 -0300

    Acessibilidade com componentes Angular

---
+++ acessibilidadeapp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    YesNoButtonGroupModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
